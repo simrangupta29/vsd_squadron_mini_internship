6324495bfcb72fb30b8fb06e4c1e839930cfd6a9
simrangupta29 <130252328+simrangupta29@users.noreply.github.com>
2024-05-09 11:23:37 +0530

    Update simran_rv32i_tb.v

--- simran_rv32i_tb.v
+++ simran_rv32i_tb.v
@@ -1,9 +1,9 @@
-module iiitb_rv32i_tb;
+module simran_rv32i_tb;
 
 reg clk,RN;
 wire [31:0]WB_OUT,NPC;
 
-iiitb_rv32i rv32(clk,RN,NPC,WB_OUT);
+simran_rv32i rv32(clk,RN,NPC,WB_OUT);
 
 
 always #3 clk=!clk;
@@ -12,7 +12,7 @@ initial begin
 RN  = 1'b1;
 clk = 1'b1;
 
-$dumpfile ("iiitb_rv32i.vcd"); //by default vcd
+  $dumpfile ("simran_rv32i.vcd"); //by default vcd
 $dumpvars (0, iiitb_rv32i_tb);
   
   #5 RN = 1'b0;
